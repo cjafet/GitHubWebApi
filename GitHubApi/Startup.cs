4bdf679eaaa014c50432ae9e845eae9081837e22
Carlos Jafet Neto <carlos.jafet@muvmobile.com>
2019-05-02 19:33:20 -0300

    Add connection for production.

--- GitHubApi/Startup.cs
+++ GitHubApi/Startup.cs
@@ -26,7 +26,7 @@ namespace GitHubApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("Production")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
